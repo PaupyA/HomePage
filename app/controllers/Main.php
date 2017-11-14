f57e801b84af39666d35e9cf78d1de2f52483b3b
Paupy <Anthony.paupy@sts-sio-caen.info>
2017-11-14 11:57:20 +0100

    Mise en place moteur de recherche fixe (google)

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -19,19 +19,42 @@ class Main extends ControllerBase {
 
     public function index() {
         $semantic = $this->jquery->semantic();
-
-        $frm=$semantic->smallLogin("frm2-4");
-        $frm->fieldAsSubmit("submit","blue fluid","sTest/dePost","#frm2-4-submit");
-        $bt=$semantic->htmlButton("bt1","S'identifier","blue","$('#modal-frm2-4').modal('show');");
+        
+        $frm = $semantic->smallLogin("frm2-4");
+        $frm->fieldAsSubmit("submit", "blue fluid", "sTest/dePost", "#frm2-4-submit");
+        $bt = $semantic->htmlButton("bt1", "S'identifier", "blue", "$('#modal-frm2-4').modal('show');");
         $bt->addIcon("sign in");
 
         echo $frm->asModal();
         echo $bt;
-
-        $frmSearch=$semantic->htmlForm("frmSearch");
-        $frmSearch->addInput("search","","","","Rechercher...");
+//        $idMoteur = DAO::getOne("models\moteur",$id );
+//        $google="https://www.google.fr/search?q";
+//        $ecosia="https://www.ecosia.org/search?q=";
+//        
+//        $moteur="";
+//        
+//        if ($idMoteur == "1" ){
+//            $moteur = $google;
+//        }
+//        Elseif ($idMoteur == "2") {
+//            $moteur = $ecosia;
+//        };
+        
+        $frmSearch = $semantic->htmlForm("frmSearch");
+        $frmSearch->addInput("q", "", "", "", "Rechercher...");
+        $frmSearch->addButton("submit", "Go");
+        $frmSearch->setProperty("action", "https://www.google.fr/search?q");
+        $frmSearch->setProperty("method", "get");
 
         $this->jquery->compile($this->view);
         $this->loadView("index.html");
     }
+
+//    public function recherche() {
+//        $recherche = "";
+//        $recherche = "https://www.google.fr/search?q=google&oq=" . $_POST["search"];
+//        $this->jquery->get($recherche,"#searchResponse");
+//        echo $this->jquery->compile($this->view);
+//    }
+
 }
